fe96b530587dbaec8e2efa2336c7d63894ef21eb
Mel-Lima <melnlima@alu.ufc.br>
2025-07-14 14:52:11 -0300

    feat: refaz o banco de dados para consertar vazamento de key

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBJ4dM6zdmTc1YzSC7SeChfZ6UnAiiWkZ0",
-  authDomain: "urucum-react-project-efd56.firebaseapp.com",
-  projectId: "urucum-react-project-efd56",
-  storageBucket: "urucum-react-project-efd56.firebasestorage.app",
-  messagingSenderId: "957567489545",
-  appId: "1:957567489545:web:d4759d45fc93891ab04ad8"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import TelaCadastro from "./paginas/TelaCadastro";
